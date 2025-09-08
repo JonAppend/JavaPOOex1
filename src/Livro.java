b55fec5a5888789f79441936974b31de977b3431
madureira <jamesonmadureira2@gmail.com>
2025-09-07 22:42:32 -0300

    Exercicio 01 (POO/Slide 01)

--- src/Livro.java
+++ src/Livro.java
@@ -4,6 +4,7 @@ public class Livro {
     public String titulo;
     public int numero_pag;
     public double preco;
+    public double novoPreco;
 
     public Livro(int ano, String autor, String titulo, int numero_pag, double preco){
         this.ano = ano;
@@ -12,7 +13,18 @@ public class Livro {
         this.numero_pag = numero_pag;
         this.preco = preco;
         System.out.println("O livro "  + titulo +  " foi registrado com sucesso.");
-
+    }
+    void exibirLivros(){
+        System.out.println("O titulo do Livro é: " + titulo + " O autor é: " + autor + " lançado em: " + ano + " e o numero de páginas é: " + numero_pag + " e o preço é: R$ " + preco);
+    }
+    void setPreco(double novoPreco){
+        if (novoPreco > 0){
+            System.out.println("------------------------------");
+            System.out.println("PREÇO ALTERADO COM SUCESSO");
+            this.preco = novoPreco;
+        }else{
+            System.out.println("Digite um valor positivo, não será aceito valores negativos.");
+        }
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        new Livro(1997, "JK Rowling", "Harry Potter e a Pedra Filosofal", 264, 35.48);
+        Livro l1 = new Livro(1997, "JK Rowling", "Harry Potter e a Pedra Filosofal", 264, 35.48);
+        Livro l2 = new Livro(1949, "George O.", "1984", 416, 60.00);
+
+        l1.exibirLivros();
+        l2.exibirLivros();
+
+        l1.setPreco(50.00);
+        l1.exibirLivros();
+
     }
 }
